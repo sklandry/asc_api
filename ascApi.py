19f1d81a69b970375f447d1e05257ec3929b7106
sklandry <skoffilandry@gmail.com>
2025-01-07 11:39:42 -0600

    Refactored

--- ascApi.py
+++ ascApi.py
@@ -11,6 +11,6 @@ def get_events():
     return jsonify(events)
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run()
+    # app.run(host='0.0.0.0', port=8000, debug=True)
 
